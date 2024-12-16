61f1259977ca16ed9283c2347a51c059ae4d3b10
lby9906 <lby9906@naver.com>
2024-12-16 18:57:19 +0900

    week01

---
+++ src/Algorithm/week01/pg_12932.java
@@ -0,0 +1,19 @@
+package Algorithm.week01;
+
+import java.util.Scanner;
+
+public class pg_12932 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        String strNum = Integer.toString(num);
+        String[] result = strNum.split("");
+        int[] arr = new int[strNum.length()];
+
+        for (int i=0; i< result.length; i++){
+            arr[i] = Integer.parseInt(result[result.length-1-i]);
+            System.out.println(arr[i]);
+        }
+    }
+}
